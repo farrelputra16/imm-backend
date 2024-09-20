3cfbd37d7cdb50154d5c3faead809a78681cb6de
revorahmat@gmail.com <revorahmat@gmail.com>
2024-09-20 14:58:27 +0700

    Add TeamController and Team model for managing team members in a company

---
+++ database/migrations/2024_09_20_073643_create_team_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->foreignId('people_id')->constrained('people')->onDelete('cascade');
+            $table->string('position')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team');
+    }
+}
