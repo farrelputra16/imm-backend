5b801c650ce248e3b2b6ca9a505b2d7036a1bf2a
revorahmat@gmail.com <revorahmat@gmail.com>
2024-10-14 14:57:13 +0700

    Refactor EventController to handle image uploads and updates

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,14 +6,13 @@ use App\Models\User;
 use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Log; // Import Log facade
+use Illuminate\Support\Facades\Log;
 
 class EventController extends Controller
 {
     public function index()
     {
         $events = Event::withCount('users')->get();
-        // Loop untuk mendapatkan setiap image yang disimpan di storage untuk cover dan hero
         foreach ($events as $event) {
             $event->cover_img = Storage::url('public/img/' . $event->cover_img);
             $event->hero_img = Storage::url('public/img/' . $event->hero_img);
@@ -35,59 +34,52 @@ class EventController extends Controller
         return view('events.edit', compact('event', 'users', 'eventUsers'));
     }
 
-
     public function store(Request $request)
     {
-        // Validate request data
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
+            'allowed_participants' => 'nullable|string|max:255',
+            'expected_participants' => 'nullable|integer',
+            'fee_type' => 'required|in:Free,Paid',
+            'organizer_name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'nomor_tlpn' => 'required|string|max:255',
             'topic' => 'nullable|string|max:255',
             'location' => 'required|string|max:255',
             'start' => 'required|date',
-            'end' => 'required|date',
-            'deadline' => 'required|date',
+            'event_duration' => 'required|string|max:255',
             'users' => 'array|exists:users,id',
-            'cover_img' => 'required|image|mimes:jpeg,png,jpg,gif|max:10000',
-            'hero_img' => 'required|image|mimes:jpeg,png,jpg,gif|max:10000',
+            'cover_img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:10000',
+            'hero_img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:10000',
         ]);
 
-        // Handle file upload menggatinya agar di store di storage supaya bisa diakses dari frontend
-        $coverImageName = time() . 'cover' . '.' . $request->cover_img->extension();
-        // $request->cover_img->move(public_path('img'), $coverImageName);
-        $request->file('cover_img')->storeAs('public/img', $coverImageName);
+        if ($request->hasFile('cover_img')) {
+            $coverImageName = time() . 'cover' . '.' . $request->cover_img->extension();
+            $request->file('cover_img')->storeAs('public/img', $coverImageName);
+            $validatedData['cover_img'] = $coverImageName;
+        }
 
+        if ($request->hasFile('hero_img')) {
+            $heroImageName = time() . 'hero' . '.' . $request->hero_img->extension();
+            $request->file('hero_img')->storeAs('public/img', $heroImageName);
+            $validatedData['hero_img'] = $heroImageName;
+        }
 
-        $heroImageName = time() . 'hero' . '.' . $request->hero_img->extension();
-        // $request->hero_img->move(public_path('img'), $heroImageName);
-        $request->file('hero_img')->storeAs('public/img', $heroImageName);
-
-        // Create and save the event
         $event = new Event();
-        $event->title = $validatedData['title'];
-        $event->description = $validatedData['description'];
-        $event->topic = $validatedData['topic'];
-        $event->location = $validatedData['location'];
-        $event->start = $validatedData['start'];
-        $event->end = $validatedData['end'];
-        $event->deadline = $validatedData['deadline'];
-        $event->cover_img = $coverImageName;
-        $event->hero_img = $heroImageName;
-
-        // Save the event and log its ID
+        $event->fill($validatedData);
         $event->save();
+
         Log::info('Event created with ID: ' . $event->id);
 
-        // Attach users to the event
         if (!empty($validatedData['users'])) {
+            $event->users()->attach($validatedData['users']);
             foreach ($validatedData['users'] as $userId) {
-                $event->users()->attach($userId);
-                Log::info('User ' . $userId . ' attached to event.');
+                Log::info('User   ' . $userId . ' attached to event.');
             }
         } else {
             Log::info('No users attached to event.');
         }
-
 
         return redirect()->route('events.index')->with('success', 'Event created successfully.');
     }
@@ -99,50 +91,55 @@ class EventController extends Controller
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
+            'allowed_participants' => 'nullable|string|max:255',
+            'expected_participants' => 'nullable|integer',
+            'fee_type' => 'required|in:Free,Paid',
+            'organizer_name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'nomor_tlpn' => 'required|string|max:255',
             'topic' => 'nullable|string|max:255',
             'location' => 'required|string|max:255',
+            'start' => 'required|date|after_or_equal:today',
+            'event_duration' => 'required|regex:/^\d{2}\.\d{2} - \d{2}\.\d{2}$/',
+            'users' => 'array|exists:users,id',
             'cover_img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:10000',
             'hero_img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:10000',
-            'start' => 'required|date',
-            'end' => 'required|date',
-            'deadline' => 'required|date',
-            'users' => 'array|exists:users,id',
         ]);
 
-        // Update the event with validated data
         $event->update($validatedData);
 
-        // Handle image update if provided
         if ($request->hasFile('cover_img')) {
-            // Delete old image if exists
-            if ($event->cover_img && file_exists(public_path('img/' . $event->cover_img))) {
-                unlink(public_path('img/' . $event->cover_img));
+            if ($event->cover_img && Storage::disk('local')->exists('public/img/' . $event->cover_img)) {
+                Storage::disk('local')->delete('public/img/' . $event->cover_img);
             }
 
-            // Move and save new image
             $coverImageName = time() . '.' . $request->cover_img->extension();
-            $request->cover_img->move(public_path('img'), $coverImageName);
+            $request->cover_img->storeAs('public/img', $coverImageName);
             $event->cover_img = $coverImageName;
+        } elseif ($event->cover_img) {
+            // Hapus gambar lama jika tidak ada perubahan gambar
+            Storage::disk('local')->delete('public/img/' . $event->cover_img);
+            $event->cover_img = null;
         }
 
         if ($request->hasFile('hero_img')) {
-            // Delete old image if exists
-            if ($event->hero_img && file_exists(public_path('img/' . $event->hero_img))) {
-                unlink(public_path('img/' . $event->hero_img));
+            if ($event->hero_img && Storage::disk('local')->exists('public/img/' . $event->hero_img)) {
+                Storage::disk('local')->delete('public/img/' . $event->hero_img);
             }
 
-            // Move and save new image
             $heroImageName = time() . '.' . $request->hero_img->extension();
-            $request->hero_img->move(public_path('img'), $heroImageName);
+            $request->hero_img->storeAs('public/img', $heroImageName);
             $event->hero_img = $heroImageName;
+        } elseif ($event->hero_img) {
+            // Hapus gambar lama jika tidak ada perubahan gambar
+            Storage::disk('local')->delete('public/img/' . $event->hero_img);
+            $event->hero_img = null;
         }
 
-        // Sync users associated with the event
         if (isset($validatedData['users'])) {
             $event->users()->sync($validatedData['users']);
         }
 
-        // Save the updated event
         $event->save();
 
         return redirect()->route('events.index')->with('success', 'Event updated successfully.');
@@ -162,20 +159,16 @@ class EventController extends Controller
     {
         $event = Event::findOrFail($id);
 
-        // Cek dan hapus file gambar cover jika ada
         if ($event->cover_img && Storage::disk('local')->exists('public/img/' . $event->cover_img)) {
             Storage::disk('local')->delete('public/img/' . $event->cover_img);
         }
 
-        // Cek dan hapus file gambar hero jika ada
         if ($event->hero_img && Storage::disk('local')->exists('public/img/' . $event->hero_img)) {
             Storage::disk('local')->delete('public/img/' . $event->hero_img);
         }
 
-        // Hapus event dari database
         $event->delete();
 
         return redirect()->route('events.index')->with('success', 'Event deleted successfully.');
     }
-
 }
